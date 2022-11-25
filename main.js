3591c6a38d4c892f4f557d79145c4d1efc93511c
fabianodsa <fabyanoduarte@hotmail.com>
2022-11-24 22:54:36 -0300

    Projeto finalizado

--- main.js
+++ main.js
@@ -1,14 +1,26 @@
 const turnOn = document.getElementById('turnOn');
 const turnOff = document.getElementById('turnOff');
 const lamp = document.getElementById('lamp');
+const reset = document.getElementById('redefining');
 
 turnOn.addEventListener('click', lampOn);
 turnOff.addEventListener('click', lampOff);
+reset.addEventListener('click', resetar);
+lamp.addEventListener('dblclick', lampBroken);
+
 
 function lampOn () {
     lamp.src = './img/ligada.jpg'
 }
 
 function lampOff () {
+    lamp.src = './img/desligada.jpg'
+}
+
+function lampBroken (){
+    lamp.src = './img/quebrada.jpg'
+}
+
+function resetar () {
     lamp.src = './img/desligada.jpg'
 }
